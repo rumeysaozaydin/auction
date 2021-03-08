a42982257102987356eb5e3fb61aa7227252c70a
Efe Eroğlu <efe.eroglu1996@gmail.com>
2021-03-08 22:48:45 +0300

    klasör düzenlemesi devam

---
+++ back/bidit/src/main/java/com/alfa/bidit/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+
+
+package com.alfa.bidit.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+
+public abstract class BaseModel implements Serializable{
+
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    @Column(name = "created_by",length = 100)
+    private String createdBy;
+    @Column(name = "updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+    @Column(name = "updated_by",length = 100)
+    private String updatedBy;
+
+}
