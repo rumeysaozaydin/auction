20eb3c3eff85f2696befed5d1a3a7b13d383fd90
Efe Eroğlu <efe.eroglu1996@gmail.com>
2021-03-09 00:40:09 +0300

    klasör düzenlemesi devam

---
+++ back/bidit/src/main/java/com/alfa/bidit/model/User.java
@@ -0,0 +1,50 @@
+package com.alfa.bidit.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "users", indexes = {@Index(name = "emailInd", columnList = "email")})
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class User extends BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name= "uname",length = 100,unique = true,nullable = false)
+    private String username;
+
+    @Column(name = "email",length = 100,nullable = false)
+    private String email;
+
+
+    @Column(name = "pwd",length = 200,nullable = false)
+    private String password;
+
+    @Column(name = "name_surname",length = 200)
+    private String nameSurname;
+
+    @Column(name = "contactInfo")
+    private String contactInfo;
+
+
+
+    @Column(name = "image")
+    private String image;
+
+
+    @Column(name = "bannedBy")
+    private String bannedBy;
+
+
+    @Column(name = "cv")
+    private String cv;
+
+}
