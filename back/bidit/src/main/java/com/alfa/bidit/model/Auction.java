3965cae4d17fa667e84fb5c2463660a920b15b7d
ahmetserdargurbuz <ahmetserdargurbuz@gmail.com>
2021-03-10 23:44:41 +0300

    Auction Model
    - Model package ina Auction class ini ekler.
    - Utils/Constants ekler.
    - AuctionStatus enum ini ekler.

---
+++ back/bidit/src/main/java/com/alfa/bidit/model/Auction.java
@@ -0,0 +1,47 @@
+package com.alfa.bidit.model;
+
+import com.alfa.bidit.utils.Constants.AuctionStatus;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "auctions")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Auction extends BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "starting_time", nullable = false)
+    private Date startingTime;
+
+    @Column(name = "expiration_time", nullable = false)
+    private Date expirationTime;
+
+    @Column(name = "title", length = 100, nullable = false)
+    private String title;
+
+    @Column(name = "description", length = 400)
+    private String description;
+
+    @Column(name = "seller_id", nullable = false)
+    private Long sellerID;
+
+    @Column(name = "initial_price", nullable = false)
+    private Long initialPrice;
+
+    @Column(name = "highest_bid")
+    private String highestBid;
+
+    @Column(name = "status")
+    private AuctionStatus status;
+
+}
---
+++ back/bidit/src/main/java/com/alfa/bidit/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.alfa.bidit.utils;
+
+public class Constants {
+
+    public enum AuctionStatus {
+        ACTIVE, EXPIRED, CANCELED
+    }
+
+}
