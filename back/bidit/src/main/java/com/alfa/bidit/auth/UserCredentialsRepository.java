ca50d322f64433b23fd01cc378d2e49c29b66d3f
rumeysa <rumeysaozaydin@hotmail.com>
2021-04-09 00:35:28 +0300

    user creddentials fix

--- back/bidit/src/main/java/com/alfa/bidit/auth/UserCredentialsRepository.java
+++ back/bidit/src/main/java/com/alfa/bidit/auth/UserCredentialsRepository.java
@@ -1,7 +1,9 @@
 package com.alfa.bidit.auth;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserCredentialsRepository extends CrudRepository<UserCredentials, Integer> {
     UserCredentials findByUsername(String username);
 }
