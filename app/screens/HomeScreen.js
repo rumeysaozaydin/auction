7f78aa198e123da82eae30b805c0e074b4b1fb1b
rumeysa <rumeysaozaydin@hotmail.com>
2021-04-14 23:15:38 +0300

    Login/Register backend connection issue #48

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -9,51 +9,54 @@ import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import { ScreenContainer } from 'react-native-screens';
 import {AuthContext} from '../context/AuthContext';
 import axios from 'axios';
-
+import {useGet} from '../hooks/useGet';
 
 const HomeScreen = ({navigation}) => {
 
-    const [auctionList,setAuctionList]  = React.useState([
-        {
-            uri: "https://moderndiplomacy.eu/wp-content/uploads/2019/01/rolex-oyster.jpg",
-            name:"Rolex Watch",
-            entryPrice:"$690",
-            bids: [720, 723, 760, 815],
-            lastBid:"$845",
-            auctionDeadline:"12.01.2021",
-            id: '3'
-        },
-        {
-            uri: "https://static.keymusic.com/products/265499/XL/yamaha-psr-e463.jpg",
-            name:"Keyboard - Yamaha PSR",
-            entryPrice:"$170",
-            bids: [180, 190, 212, 225, 245],
-            lastBid:"$260",
-            auctionDeadline:"13.01.2021",
-            id: '2'
-        },
-        {
-            uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2gR6EMYNpytjkjEIxXBAFCyEkjUWTV7eC2Q&usqp=CAU",
-            name:"PS4 + FIFA 2019",
-            entryPrice:"$260",
-            bids:[263],
-            lastBid:"$265",
-            auctionDeadline:"14.01.2021",
-            id: '1'
-        }
-      ]
-    );
+    // const [auctionList, setAuctionList]  = React.useState([
+    //     {
+    //         uri: "https://moderndiplomacy.eu/wp-content/uploads/2019/01/rolex-oyster.jpg",
+    //         name:"Rolex Watch",
+    //         entryPrice:"$690",
+    //         bids: [720, 723, 760, 815],
+    //         lastBid:"$845",
+    //         auctionDeadline:"12.01.2021",
+    //         id: '3'
+    //     },
+    //     {
+    //         uri: "https://static.keymusic.com/products/265499/XL/yamaha-psr-e463.jpg",
+    //         name:"Keyboard - Yamaha PSR",
+    //         entryPrice:"$170",
+    //         bids: [180, 190, 212, 225, 245],
+    //         lastBid:"$260",
+    //         auctionDeadline:"13.01.2021",
+    //         id: '2'
+    //     },
+    //     {
+    //         uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2gR6EMYNpytjkjEIxXBAFCyEkjUWTV7eC2Q&usqp=CAU",
+    //         name:"PS4 + FIFA 2019",
+    //         entryPrice:"$260",
+    //         bids:[263],
+    //         lastBid:"$265",
+    //         auctionDeadline:"14.01.2021",
+    //         id: '1'
+    //     }
+    //   ]
+    // );
+    const auctionList = useGet('/auctions/all');
+
+  
     let auctionCards = auctionList.map((auction, index) => {
         return (
         <AuctionCard
             isFavourite={false}
             key = {index}
             uri={auction.uri} 
-            name={auction.name} 
-            entryPrice={auction.entryPrice}
+            name={auction.title} 
+            entryPrice={auction.initialPrice}
             bids={auction.bids}
-            lastBid={auction.lastBid} 
-            auctionDeadline={auction.auctionDeadline}
+            lastBid={auction.highestBid} 
+            auctionDeadline={auction.expirationTime}
             navigation={navigation}
             id={auction.id}
         />
@@ -121,7 +124,6 @@ const HomeScreen = ({navigation}) => {
       </ScreenContainer>
       );
 };
-
 
 const styles = StyleSheet.create({
     container: {
