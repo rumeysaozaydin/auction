a05aeb61a2c24830cab8e8781c2a2c5709fe7828
Efe Eroğlu <efe.eroglu1996@gmail.com>
2021-04-24 01:48:17 +0300

    notif servisi duzeltildi

--- back/bidit/src/main/java/com/alfa/bidit/controller/NotifController.java
+++ back/bidit/src/main/java/com/alfa/bidit/controller/NotifController.java
@@ -1,140 +1,47 @@
 package com.alfa.bidit.controller;
 
 
+import com.alfa.bidit.dto.notificationInfo;
+import com.alfa.bidit.model.User;
+import com.alfa.bidit.service.impl.NotificationServiceImpl;
 import com.alfa.bidit.utils.ApiPaths;
-import io.github.jav.exposerversdk.ExpoPushMessage;
-import io.github.jav.exposerversdk.ExpoPushMessageTicketPair;
-import io.github.jav.exposerversdk.ExpoPushReceipt;
-import io.github.jav.exposerversdk.ExpoPushTicket;
-import io.github.jav.exposerversdk.PushClient;
+
 import io.github.jav.exposerversdk.PushClientException;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(ApiPaths.NotificationController.notif)
 @Api(value = ApiPaths.NotificationController.notif)
 public class NotifController {
 
+   private final NotificationServiceImpl notificationServiceImpl;
 
+
+    public NotifController(NotificationServiceImpl notificationServiceImpl) {
+        this.notificationServiceImpl = notificationServiceImpl;
+    }
 
     @GetMapping(value = "/{pushToken}/{title}/{message}")
     public void deneme(@PathVariable("pushToken") String pushToken,@PathVariable("title") String title,@PathVariable("message") String message) throws PushClientException, InterruptedException {
        // sendNotif("ExponentPushToken[pymoyhG9rYlFIUUFrZP5tf]","başlıkov","messageov");
-        sendNotif(pushToken,title,message);
+        notificationServiceImpl.sendNotif(pushToken,title,message);
 
     }
 
     @GetMapping(value = "/testo")
     public void denemov() throws PushClientException, InterruptedException {
-         sendNotif("ExponentPushToken[pymoyhG9rYlFIUUFrZP5tf]","başlıkov","messageov");
+        notificationServiceImpl.sendNotif("ExponentPushToken[pymoyhG9rYlFIUUFrZP5tf]","başlıkov","messageov");
        // sendNotif(pushToken,title,message);
 
     }
 
-
-
-
-    public void sendNotif(String recipient,String title,String message) throws PushClientException, InterruptedException {
-      //  String recipient = "ExponentPushToken[pymoyhG9rYlFIUUFrZP5tf]"; // To test, you must replace the recipient with a valid token!
-      //  String title = "My message title!";
-      //  String message = "A push message from ExampleExpoServer";
-
-        if (!PushClient.isExponentPushToken(recipient))
-            throw new Error("Token:" + recipient + " is not a valid token.");
-
-        ExpoPushMessage expoPushMessage = new ExpoPushMessage();
-        expoPushMessage.getTo().add(recipient);
-        expoPushMessage.setTitle(title);
-        expoPushMessage.setBody(message);
-
-        List<ExpoPushMessage> expoPushMessages = new ArrayList<>();
-        expoPushMessages.add(expoPushMessage);
-
-        PushClient client = new PushClient();
-        List<List<ExpoPushMessage>> chunks = client.chunkPushNotifications(expoPushMessages);
-
-        List<CompletableFuture<List<ExpoPushTicket>>> messageRepliesFutures = new ArrayList<>();
-
-        for (List<ExpoPushMessage> chunk : chunks) {
-            messageRepliesFutures.add(client.sendPushNotificationsAsync(chunk));
-        }
-
-        // Wait for each completable future to finish
-        List<ExpoPushTicket> allTickets = new ArrayList<>();
-        for (CompletableFuture<List<ExpoPushTicket>> messageReplyFuture : messageRepliesFutures) {
-            try {
-                for (ExpoPushTicket ticket : messageReplyFuture.get()) {
-                    allTickets.add(ticket);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
-
-        List<ExpoPushMessageTicketPair<ExpoPushMessage>> zippedMessagesTickets = client.zipMessagesTickets(expoPushMessages, allTickets);
-
-        List<ExpoPushMessageTicketPair<ExpoPushMessage>> okTicketMessages = client.filterAllSuccessfulMessages(zippedMessagesTickets);
-        String okTicketMessagesString = okTicketMessages.stream().map(
-                p -> "Title: " + p.message.getTitle() + ", Id:" + p.ticket.getId()
-        ).collect(Collectors.joining(","));
-        System.out.println(
-                "Recieved OK ticket for " +
-                        okTicketMessages.size() +
-                        " messages: " + okTicketMessagesString
-        );
-
-        List<ExpoPushMessageTicketPair<ExpoPushMessage>> errorTicketMessages = client.filterAllMessagesWithError(zippedMessagesTickets);
-        String errorTicketMessagesString = errorTicketMessages.stream().map(
-                p -> "Title: " + p.message.getTitle() + ", Error: " + p.ticket.getDetails().getError()
-        ).collect(Collectors.joining(","));
-        System.out.println(
-                "Recieved ERROR ticket for " +
-                        errorTicketMessages.size() +
-                        " messages: " +
-                        errorTicketMessagesString
-        );
-
-
-
-        System.out.println("Fetching reciepts...");
-
-        List<String> ticketIds = (client.getTicketIdsFromPairs(okTicketMessages));
-        CompletableFuture<List<ExpoPushReceipt>> receiptFutures = client.getPushNotificationReceiptsAsync(ticketIds);
-
-        List<ExpoPushReceipt> receipts = new ArrayList<>();
-        try {
-            receipts = receiptFutures.get();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(
-                "Recieved " + receipts.size() + " receipts:");
-
-        for (ExpoPushReceipt reciept : receipts) {
-            System.out.println(
-                    "Receipt for id: " +
-                            reciept.getId() +
-                            " had status: " +
-                            reciept.getStatus());
-
-        }
-
-
+    @PostMapping(value = "/sendNotifWithBody")
+    public void denemovBody(@RequestBody notificationInfo notificationInfo) throws PushClientException, InterruptedException {
+        notificationServiceImpl.sendNotif(notificationInfo.getTokenName(),notificationInfo.getTitle(),notificationInfo.getMessage());
+        // sendNotif(pushToken,title,message);
 
     }
+
 }
